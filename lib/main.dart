73ee96c505912ce3a08e2f54565943eea2da5711
oElberte <89710472+oElberte@users.noreply.github.com>
2021-12-09 23:02:22 -0300

    Application base

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:shop/screens/products_overview_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shop',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ProductsOverviewScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
