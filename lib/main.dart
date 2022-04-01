b5e66924715b2caf2ff181288ba68174b1730263
oElberte <89710472+oElberte@users.noreply.github.com>
2022-04-01 15:16:03 -0300

    Product management screen

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:shop/models/product_list.dart';
 import 'package:shop/screens/cart_screen.dart';
 import 'package:shop/screens/orders_screen.dart';
 import 'package:shop/screens/product_detail_screen.dart';
+import 'package:shop/screens/products_screen.dart';
 import 'package:shop/screens/products_overview_screen.dart';
 import 'package:shop/utils/app_routes.dart';
 
@@ -46,6 +47,7 @@ class MyApp extends StatelessWidget {
           AppRoutes.productDetail: (ctx) => const ProductDetailScreen(),
           AppRoutes.cart: (ctx) => const CartScreen(),
           AppRoutes.orders: (ctx) => const OrdersScreen(),
+          AppRoutes.products: (ctx) => const ProductsScreen(),
         },
         debugShowCheckedModeBanner: false,
       ),
